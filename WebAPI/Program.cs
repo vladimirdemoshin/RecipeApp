ec19c03b6542295d43122494faa6b341b52ccb1d
vladimirdemoshin <vladimirdemoshin@gmail.com>
2024-07-28 20:20:05 +0200

    renamed and updated RecipesController

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,6 +1,10 @@
+using DataAccess.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddScoped<IRecipeRepository, RecipeRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
