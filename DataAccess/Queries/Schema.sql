e2d5c754a64eae1d78f74956dfbc1a41544479e2
vladimirdemoshin <vladimirdemoshin@gmail.com>
2025-06-01 18:46:37 +0200

    added schema code for auto creation of db

--- DataAccess/Queries/Schema.sql
+++ DataAccess/Queries/Schema.sql
@@ -1,3 +1,13 @@
+CREATE LOGIN RecipeAppLogin WITH PASSWORD = 'STRONG_PASSWORD';
+
+CREATE DATABASE RecipeAppDB;
+
+USE RecipeAppDB;
+CREATE USER RecipeAppUser FOR LOGIN RecipeAppLogin;
+
+ALTER ROLE db_datareader ADD MEMBER RecipeAppUser;
+ALTER ROLE db_datawriter ADD MEMBER RecipeAppUser;
+
 CREATE TABLE Recipes (
     RecipeId int IDENTITY(1,1) PRIMARY KEY,
     Title varchar(256),
