23e2c5fea98cda71c05c1fa9f8c26eb811a62d90
vladimirdemoshin <vladimirdemoshin@gmail.com>
2025-06-01 16:56:48 +0200

    updated Seed.sql with steps

--- DataAccess/Queries/Seed.sql
+++ DataAccess/Queries/Seed.sql
@@ -2,4 +2,11 @@ INSERT INTO Recipes
 VALUES
 ('Pizza', 'Pizza description'),
 ('Pasta with salmon', 'Delicious creamy pasta'),
-('Fries', 'Crispy fries')
+('Fries', 'Crispy fries'),
+('Salad with cucumber and tomato', 'Delicious summer salad');
+
+INSERT INTO RecipeSteps
+VALUES
+(4, 0, 'Cut tomatos and cucumbers on small cubes'),
+(4, 1, 'Then mix it with in bowl'),
+(4, 2, 'Add topping with some sour cream, salt and pepper, mix everything. Salad is ready!');
